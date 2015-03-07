867a1cc0f15d76f5f8f05f3a4a931c2d94827f78
Jonne Airaksinen <jonne.airaksinen@gmail.com>
2015-03-08 00:28:10 +0200

    routing of the nodes

--- overlay-controller.js
+++ overlay-controller.js
@@ -25,12 +25,12 @@ server.on('message', function(message, remote) {
 	// Since the message is of type Buffer, use its toString-method and trim unnecessary whitespaces.
 	var messageContent = message.toString('utf8').trim();
 
-	if (messageContent.split(' ')[0] == 'STARTED') {
+	if (messageContent.split(' ')[0] == 'NODE_STARTED') {
 
 		var pieces = messageContent.split(' ');
 
 		var node = {
-			id: pieces[1].trim(),
+			id: parseInt(pieces[1].trim()),
 			address: remote.address,
 			port: remote.port
 		}
@@ -38,17 +38,17 @@ server.on('message', function(message, remote) {
 		nodes[node.id-1] = node;
 		startedNodes += 1;
 
-		if (startedNodes % 128 == 0 || startedNodes > 950)
+		if (startedNodes % 128 == 0)
 			console.log(startedNodes + ' started');
 		
 		if (startedNodes == 1024) {
 			console.log('all nodes started');
 			console.log(nodes);
-			//routedNodes(nodes);
+			routeNodes(nodes);
 		}
 	}
 
-	if (messageContent.split(' ')[0] == 'ROUTED') {
+	if (messageContent.split(' ')[0] == 'NODE_ROUTED') {
 
 		routedNodes += 1;
 
@@ -65,9 +65,73 @@ function routeNodes(nodes) {
 	var centernodes = [];
 	var chunkSize = 128;
 	for (var i = 0; i < nodes.length; i+=chunkSize) {
-		var chunk = nodes.slice(i,i+chunkSize);
-		centernodes.push(nodes[i+chunkSize]);
+		var subTree = nodes.slice(i,i+chunkSize);
+		var subTreeCenter = nodes[i+chunkSize-1]
+		centernodes.push(subTreeCenter);
+		addToNodeRoutingTable(subtreeCenter, subtree[Math.ceil(subtree.length/2)], subtree[0].id, subtree[subtree.length-1].id);
+		routeSubtree(subtreeCenter, subTree);
 	}
+}
+
+function routeSubtree(parent, subtree) {
+
+	if (chunk.length == 1) {
+		setRoutingTableForSubtreeNode(subtree[0], parent, undefined, 0, undefined, undefined, undefined, 1024)
+		return;
+	}
+
+	var curRoot = subtree[Math.ceil(subtree.length/2)];
+
+	var leftSubTree = subtree.slice(0,curRootIndex);
+	var leftChild = leftSubtree[Math.ceil(leftSubTree.length/2)];
+
+	var rightSubTree = subtree.slice(curRootIndex+1, subtree.length);
+	var rightChild = rightSubtree[Math.ceil(rightSubTree.length/2)];
+
+	setRoutingTableForSubtreeNode(curRoot, parent, leftChild, leftSubTree[0].id, leftSubTree[leftSubTree.length-1].id, rightChild, rightSubtree[0].id, rightSubtree[rightSubtree.length-1].id);
+
+	routeSubtree(parent, leftSubtree);
+	routeSubtree(parent, rightSubtree);
+
+}
+
+function setRoutingTableForSubtreeNode(node, parent, leftChild, leftSmallest, LeftLargest, rightChild, rightSmallest, rightLargest) {
+
+	var route = 'ROUTE_ADD ';
+
+	route += generateRouteString(parent, leftSmallest, rightLargest);
+	
+	if (typeof leftChild !== undefined) {
+		route += generateRouteString(leftChild, leftSmallest, leftLargest);
+	}
+	
+	if (typeof rightChild !== undefined) {
+		route += generateRouteString(rightChild, rightSmallest, rightLargest);
+	}
+
+	sendRouteAddMessage(new Buffer(route), node);
+
+}
+
+function addToNodeRoutingTable(node, destination, smallest, largest) {
+
+	var route = 'ROUTE_ADD ';
+
+	route += generateRouteString(destination, smallest, largest);
+
+	sendRouteAddMessage(new Buffer(route), node);
+}
+
+function generateRouteString(destination, smallest, largest) {
+	return destination.id + '/' + destination.address + '/' + destination.port + '/' + smallest + '/' + largest + '|';
+}
+
+function sendRouteAddMessage(msg, node) {
+
+	server.send(msg, 0, msg.length, node.port, node.address, function (err, bytes) {
+		if (err) throw err;
+	});
+
 }
 
 function startNodes(nodes) {
--- overlay-node.js
+++ overlay-node.js
@@ -10,6 +10,7 @@ var os = require('os');
 var id = process.argv[2];
 var host = os.hostname();
 var port = 42000 + parseInt(id);
+var routes = [];
 
 // Create the UDP server
 var dgram = require('dgram');
@@ -28,7 +29,7 @@ server.on('listening', function() {
 	var address = server.address();
 	//console.log('listening on ' + address.address + ':' + address.port);
 
-	var readyMsg = new Buffer('STARTED ' + id);
+	var readyMsg = new Buffer('NODE_STARTED ' + id);
 	server.send(readyMsg, 0, readyMsg.length, controllerPort, controllerAddress, function (err, bytes) {
 		if (err) throw err;
 	});
@@ -41,18 +42,25 @@ server.on('message', function(message, remote) {
 	// Since the message is of type Buffer, use its toString-method and trim unnecessary whitespaces.
 	var messageContent = message.toString('utf8').trim();
 
-	// When the node gets a 'PING'-message, it replies with a 'PONG' to let the sender know
-	// the message went through.
-	if (messageContent == 'PING') {
+	if (messageContent.split(' ')[0] == 'ROUTE_ADD') {
+		var routesTmp = messageContent.split(' ')[1].split('|');
+		for (var i = 0; i < routesTmp.length; i++) {
+			
+			if (routesTmp[i]) {
+				
+				routePieces = routesTmp[i].split('/');
+				var route = {
+					id: routePieces[0],
+					address: routePieces[1],
+					port: routePieces[2],
+					smallest: routePieces[3],
+					largest: routePieces[4]
+				}
+				routes.push(route);
+				
+			}
 
-		var reply = new Buffer('PONG ' + host);
-		server.send(reply, 0, reply.length, remote.port, remote.address, function (err, bytes) {
-			if (err) throw err;
-		});
-	}
-
-	if (messageContent.split(' ')[0] == 'ROUTE') {
-		var neighbours = messageContent.split(' ')[1].split('/');
+		};
 	}
 	
 	if (messageContent.split(' ')[0] == 'MSG') {
@@ -60,98 +68,11 @@ server.on('message', function(message, remote) {
 		var hops = messageContent.split(' ')[2].trim();
 	}
 
-	// Receiving message from another node
-	if (messageContent.split(' ')[0] == 's') {
-		
-
-		// Pick the greater value from the local timestamp and received timestamp, add one to the chosen one.
-		clock = parseInt(senderClock) > parseInt(clock) ? parseInt(senderClock)+1 : parseInt(clock) + 1;
-		
-		var out = 'r ' + senderId + ' ' + senderClock + ' ' + clock;
-		history += ' ' + out;
-		console.log(out);
-	}
-
 });
 
 // Listen to the port on the host and port specified in the config file.
 server.bind(port, host);
 
-/**
- ** The actual run of the node, started when all other nodes are ready.
- ** The method is called on periodically, on a delay specified by a 
- ** setInterval call.
- **/
-function runProcess() {
-
-	// Random integer to decide whether to do a local event or send a message.
-	var localOrSend = randomInteger(1,2);
-
-	// Local event
-	if (localOrSend === 1) {
-		var increase = randomInteger(1,5);
-		clock += increase;
-		events += 1;
-
-		var out = 'l ' + increase;
-		history += ' ' + out;
-		console.log(out);
-	}
-	// Send message to other node, chosen randomly.
-	else if (localOrSend === 2) {
-		var receivingNode = nodes[randomInteger(0,nodes.length-1)].split(' ');
-		var receivingId = receivingNode[0];
-		var receivingHost = receivingNode[1];
-		var receivingPort = receivingNode[2];
-
-		var syncMsg = new Buffer('s ' + id + ' ' + clock);
-		server.send(syncMsg, 0, syncMsg.length, receivingPort, receivingHost, function (err, bytes) {
-			if (err) throw err;
-		});
-
-		var out = 's ' + receivingId + ' ' + clock
-		history += ' ' + out;
-		console.log(out);
-	}
-
-	// When 100 events have been reached, clear the interval and exit the process.
-	if (events === 100) {
-
-		clearInterval(running);
-		
-		// Uncomment the line below to output the whole history of this node on one line.
-		// console.log(history);
-		
-		process.exit();
-	}
-}
-
 function pingMasterNode() {
 
-}
-
-/**
- ** Pings all the other nodes to make sure they are up and listening
- ** for input before starting the actual run. The method is called 
- ** periodically, on a delay specified by a setInterval call.
- **/
-function pingOtherNodes() {
-
-	for (var i = 0; i < notReadyNodes.length; i++) {
-		if (notReadyNodes[i] != "") {
-			var pingHost = notReadyNodes[i].split(' ')[1];
-			var pingPort = notReadyNodes[i].split(' ')[2];
-			var msg = new Buffer('PING');
-			server.send(msg, 0, msg.length, pingPort, pingHost, function (err, bytes) {
-				if (err) throw err;
-			});
-		}
-	}
-}
-
-/** 
- ** Helper method for random integers.
- **/
-function randomInteger(min, max) {
-	return Math.floor(Math.random()*(max-min+1)+min);
 }
